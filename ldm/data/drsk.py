ecd56a4d87402e1ad69d5eb6210850584193a099
minha12 <minhha.x89@gmail.com>
2025-03-14 15:27:53 +0000

    refactor: replace SmallestMaxSize with Resize for forcing image and segmentation resizing

--- ldm/data/drsk.py
+++ ldm/data/drsk.py
@@ -38,16 +38,15 @@ class SegmentationBase(Dataset):
                 "bicubic": cv2.INTER_CUBIC,
                 "area": cv2.INTER_AREA,
                 "lanczos": cv2.INTER_LANCZOS4}[self.interpolation]
-            self.image_rescaler = albumentations.SmallestMaxSize(max_size=self.size,
-                                                                 interpolation=self.interpolation)
-            self.segmentation_rescaler = albumentations.SmallestMaxSize(max_size=self.size,
-                                                                        interpolation=cv2.INTER_NEAREST)
-            self.center_crop = not random_crop
-            if self.center_crop:
-                self.cropper = albumentations.CenterCrop(height=self.size, width=self.size)
-            else:
-                self.cropper = albumentations.RandomCrop(height=self.size, width=self.size)
-            self.preprocessor = self.cropper
+            
+            # Replace SmallestMaxSize with Resize to always get target size
+            self.image_rescaler = albumentations.Resize(height=self.size, width=self.size,
+                                                        interpolation=self.interpolation)
+            self.segmentation_rescaler = albumentations.Resize(height=self.size, width=self.size,
+                                                              interpolation=cv2.INTER_NEAREST)
+            
+            # No need for cropping anymore since we're directly resizing to target dimensions
+            self.preprocessor = albumentations.Compose([])  # Empty composer as we don't need additional transforms
 
     def __len__(self):
         return self._length
@@ -58,24 +57,24 @@ class SegmentationBase(Dataset):
         if not image.mode == "RGB":
             image = image.convert("RGB")
         image = np.array(image).astype(np.uint8)
-        if self.size is not None:
-            image = self.image_rescaler(image=image)["image"]
+        
         segmentation = Image.open(example["segmentation_path_"])
         assert segmentation.mode == "L", segmentation.mode
         segmentation = np.array(segmentation).astype(np.uint8)
         if self.shift_segmentation:
             # used to support segmentations containing unlabeled==255 label
             segmentation = segmentation+1
+        
         if self.size is not None:
+            # Apply resizing transforms
+            image = self.image_rescaler(image=image)["image"]
             segmentation = self.segmentation_rescaler(image=segmentation)["image"]
-        if self.size is not None:
-            processed = self.preprocessor(image=image,
-                                          mask=segmentation
-                                          )
+            
+            # Apply any additional preprocessing (empty in this case)
+            processed = self.preprocessor(image=image, mask=segmentation)
         else:
-            processed = {"image": image,
-                         "mask": segmentation
-                         }
+            processed = {"image": image, "mask": segmentation}
+            
         example["image"] = (processed["image"]/127.5 - 1.0).astype(np.float32)
         segmentation = processed["mask"]
         onehot = np.eye(self.n_labels)[segmentation]
