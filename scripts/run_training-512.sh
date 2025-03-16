b20f04a97ae220e963744138a9f1f22b33358d96
minha12 <minhha.x89@gmail.com>
2025-03-16 13:24:33 +0000

    feat: adding timestamp to training log file

--- scripts/run_training-512.sh
+++ scripts/run_training-512.sh
@@ -1,1 +1,3 @@
-nohup python main.py --base models/ldm/drsk/config-512-with-vq-f4.yaml -t --gpus 0,1,2,3 --strategy ddp > ldm_training.log 2>&1 &
+timestamp=$(date +"%Y-%m-%d_%H%M%S")
+logfile="ldm_training_512_${timestamp}.log"
+nohup python main.py --base models/ldm/drsk/config-512-with-vq-f4.yaml -t --gpus 0,1,2,3 --strategy ddp > "$logfile" 2>&1 &
